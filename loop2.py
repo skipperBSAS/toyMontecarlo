84ada480fba8e89294daf91c75651f76aac63475
mariano10792 <mariano10792@gmail.com>
2018-06-29 11:43:43 -0300

    corrijo loop2.py

--- loop2.py
+++ loop2.py
@@ -3,12 +3,23 @@
 import os,sys
 
 command = ""
-for a in range(73,86):
+for a in range(73,88):
 
 
 
-		command += " \n ./skExtract.exe -c extractConfig.xml proc_fe552018-04-12_00"+str(a)+".fits -o output_"+str(a)+".root"+" && "
+		command += " \n ./skExtract.exe -c extractConfig.xml proc_fe552018-04-12_00"+str(a)+".fits -o output_"+str(a)+".root"
 		#command += "./skExtract.exe -c extractConfig.xml interactions+dc_A="+str(a)+"_B="+str(b)+".fits -o output_"+str(a)+"_"+str(b)+".root"+" && "
+
+command = command[:-2]
+
+print command
+os.system(command)
+command = ""
+
+for a in range(87,101):
+		command += " \n ./skExtract.exe -c extractConfig.xml proc_fe552018-04-13_00"+str(a)+".fits -o output_"+str(a)+".root"
+		##command += "\n scp monsoon@estela:/home/monsoon/andre/macros/datos/tracer/newSetup/fe55/dataApril12/proc_fe552018-04-13_00"+str(a)+".fits /home/mariano/Desktop/caca/"
+		##command += "./skExtract.exe -c extractConfig.xml interactions+dc_A="+str(a)+"_B="+str(b)+".fits -o output_"+str(a)+"_"+str(b)+".root"+" && "
 
 
 command = command[:-2]
@@ -17,10 +28,12 @@ print command
 os.system(command)
 command = ""
 
-for a in range(87,100):
-		command += " \n ./skExtract.exe -c extractConfig.xml proc_fe552018-04-13_00"+str(a)+".fits -o output_"+str(a)+".root"+" && "
-		#command += "\n scp monsoon@estela:/home/monsoon/andre/macros/datos/tracer/newSetup/fe55/dataApril12/proc_fe552018-04-13_00"+str(a)+".fits /home/mariano/Desktop/caca/"
-		#command += "./skExtract.exe -c extractConfig.xml interactions+dc_A="+str(a)+"_B="+str(b)+".fits -o output_"+str(a)+"_"+str(b)+".root"+" && "
+for a in range(100,118):
+
+		command += " \n ./skExtract.exe -c extractConfig.xml proc_fe552018-04-13_0"+str(a)+".fits -o output_"+str(a)+".root"
+
+		##command += "\n scp monsoon@estela:/home/monsoon/andre/macros/datos/tracer/newSetup/fe55/dataApril12/proc_fe552018-04-13_0"+str(a)+".fits /home/mariano/Desktop/caca/"
+		##command += "./skExtract.exe -c extractConfig.xml interactions+dc_A="+str(a)+"_B="+str(b)+".fits -o output_"+str(a)+"_"+str(b)+".root"+" && "
 
 
 command = command[:-2]
@@ -29,26 +42,12 @@ print command
 os.system(command)
 command = ""
 
-for a in range(100,117):
+for a in range(117,268):
 
-		command += " \n ./skExtract.exe -c extractConfig.xml proc_fe552018-04-13_0"+str(a)+".fits -o output_"+str(a)+".root"+" && "
+		command += " \n ./skExtract.exe -c extractConfig.xml proc_fe552018-04-14_0"+str(a)+".fits -o output_"+str(a)+".root"
 
-		#command += "\n scp monsoon@estela:/home/monsoon/andre/macros/datos/tracer/newSetup/fe55/dataApril12/proc_fe552018-04-13_0"+str(a)+".fits /home/mariano/Desktop/caca/"
-		#command += "./skExtract.exe -c extractConfig.xml interactions+dc_A="+str(a)+"_B="+str(b)+".fits -o output_"+str(a)+"_"+str(b)+".root"+" && "
-
-
-command = command[:-2]
-
-print command
-os.system(command)
-command = ""
-
-for a in range(117,267):
-
-		command += " \n ./skExtract.exe -c extractConfig.xml proc_fe552018-04-14_0"+str(a)+".fits -o output_"+str(a)+".root"+" && "
-
-		command += "\n scp monsoon@estela:/home/monsoon/andre/macros/datos/tracer/newSetup/fe55/dataApril12/proc_fe552018-04-14_0"+str(a)+".fits /home/mariano/Desktop/caca/"
-		#command += "./skExtract.exe -c extractConfig.xml interactions+dc_A="+str(a)+"_B="+str(b)+".fits -o output_"+str(a)+"_"+str(b)+".root"+" && "
+		##command += "\n scp monsoon@estela:/home/monsoon/andre/macros/datos/tracer/newSetup/fe55/dataApril12/proc_fe552018-04-14_0"+str(a)+".fits /home/mariano/Desktop/caca/"
+		##command += "./skExtract.exe -c extractConfig.xml interactions+dc_A="+str(a)+"_B="+str(b)+".fits -o output_"+str(a)+"_"+str(b)+".root"+" && "
 
 
 command = command[:-2]
